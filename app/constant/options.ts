37f0c114e06960bb70adc85b743292a0c4d1dfee
dat <dat277@gmail.com>
2025-03-14 17:48:58 +0700

    feat: add options

---
+++ app/constant/options.ts
@@ -0,0 +1,19 @@
+export const alignmentOptions = [
+  { label: 'Left', value: 'left' },
+  { label: 'Center', value: 'center' },
+  { label: 'Right', value: 'right' },
+];
+export const numberingOptions = [
+  { label: 'numbers', value: 'numbers' },
+  { label: 'dots', value: 'dots' },
+  { label: 'none', value: 'none' },
+];
+export const sectionLineOptions = [
+  { label: 'Off', value: 'off' },
+  { label: 'On', value: 'on' },
+];
+export const linkHoverStyleOptions = [
+  { label: 'None', value: 'none' },
+  { label: 'Underline (Solid)', value: 'solid' },
+  { label: 'Underline (Dotted)', value: 'dotted' },
+];
